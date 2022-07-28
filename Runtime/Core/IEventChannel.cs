a11f6d96e8158b0f3ad86cbb8e5e59b25db4c1e4
Zaljin <jottossonta@gmail.com>
2022-07-28 16:06:57 +0200

    - Added missing eventchannel interface changes.

--- Runtime/Core/IEventChannel.cs
+++ Runtime/Core/IEventChannel.cs
@@ -7,6 +7,14 @@ namespace UCommon.EventChannels
     /// </summary>
     public interface IEventChannel
     {
-        void Trigger(); // Raises event.
+        void Emit(); // Raises event.
+    }
+
+    /// <summary>
+    /// Interface for event channels of type void.
+    /// </summary>
+    public interface IEventChannel<T1>
+    {
+        void Emit(T1 value); // Raises event.
     }
 }
